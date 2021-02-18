9b7342d7b966d282f0e88e408c56a1b9197a00df
rlawhdcjf333 <77156710+rlawhdcjf333@users.noreply.github.com>
2021-02-18 13:12:29 +0900

    queue로 이벤트관리자 수정
    vector에서 queue로
    
    Q.empty() == if(!Q.size()) return true;
    Q.front() 최전방 리턴
    Q.pop() 최전방 삭제
    Q.push() 최후방 추가
    
    while(!mEventQueue.empty()) 라는 구문은 이벤트 큐가 비어있지 않을 때 조건식이 true이므로  --> 이벤트 큐가 비어있을 때까지 계속 반복하게 됨

--- Practice/01_WinMain/GameEventManager.cpp
+++ Practice/01_WinMain/GameEventManager.cpp
@@ -14,25 +14,27 @@ GameEventManager::~GameEventManager()
 
 void GameEventManager::RemoveAllEvent()
 {
-	for (int i = 0; i < mEventQueue.size(); ++i)
-		SafeDelete(mEventQueue[i]);
-	mEventQueue.clear();
+	while(!mEventQueue.empty()){
+
+		SafeDelete(mEventQueue.front());
+		mEventQueue.pop();
+	}
 }
 
 void GameEventManager::Update()
 {
-	if (mEventQueue.size() == 0)
+	if (mEventQueue.empty())
 		return;
 
-	bool isEnd = mEventQueue[0]->Update();
+	bool isEnd = mEventQueue.front()->Update();
 	if (isEnd == true)
 	{
-		SafeDelete(mEventQueue[0]);
-		mEventQueue.erase(mEventQueue.begin());
+		SafeDelete(mEventQueue.front());
+		mEventQueue.pop();
 
 		if (mEventQueue.size() != 0)
 		{
-			mEventQueue[0]->Start();
+			mEventQueue.front()->Start();
 		}
 	}
 }
@@ -43,5 +45,5 @@ void GameEventManager::PushEvent(IEvent * event)
 	{
 		event->Start();
 	}
-	mEventQueue.push_back(event);
+	mEventQueue.push(event);
 }
--- Practice/01_WinMain/GameEventManager.h
+++ Practice/01_WinMain/GameEventManager.h
@@ -4,7 +4,7 @@ class GameEventManager
 	Singleton(GameEventManager)
 private:
 	//얘 queue라는 자료구조 공부해서 queue로 바꿔오기
-	vector<class IEvent*> mEventQueue;
+	queue <class IEvent*> mEventQueue;
 public:
 	GameEventManager();
 	~GameEventManager();
